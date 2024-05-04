ad4cd58b00e28ecabb5e11c1679445705f53ae52
Lorenzo <lorenzo@nioel.com>
2024-05-04 23:30:50 +1000

    chore: add db sql

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+-- Use pgcrypto extension for UUID support.
+CREATE EXTENSION "pgcrypto";
+
+CREATE TABLE "user" (
+  "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  "created_timestamp_unix" bigint NOT NULL,
+  "email" text UNIQUE NOT NULL
+);
+
+CREATE TABLE "otp" (
+  "id" serial PRIMARY KEY,
+  "value" integer NOT NULL,
+  "user_email" text NOT NULL,
+  "exp_timestamp_unix" integer NOT NULL,
+  "used_timestamp_unix" integer
+);
